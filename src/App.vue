8624d73cf3927cc560706be6d9f18b84f5e4c1ea
zhyiwen <zhang92107@sina.com>
2019-02-14 16:40:45 +0800

    添加设置按钮 修改侧边栏图标

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,42 @@
         <a id="leftmenu-trigger" class="pull-left head-logo" title="收起/展开导航栏">
           <i class="fa fa-align-left"></i>
         </a>
-        <h3 class="pull-left head-logo">
-          ELEMENT 示例
-        </h3>
+        <h3 class="pull-left head-logo">ELEMENT 示例</h3>
         <a id="rightmenu-trigger" class="pull-right head-logo" title="收起/展开导航栏">
           <i class="fa fa-align-right"></i>
         </a>
         <div class="head-user pull-right">
           <span>抓耗子的猫</span>
-          <img src="./assets/user.jpg" alt="">
+          <img src="./assets/user.jpg" alt>
         </div>
         <div class="pull-right head-tool-group">
-          <a class="head-tool"><i class="fa fa-fw fa-level-down"></i></a>
-          <a class="head-tool"><i class="fa fa-fw fa-tasks"></i></a>
-          <a class="head-tool"><i class="fa fa-fw fa-bell"></i></a>
-          <a class="head-tool"><i class="fa fa-fw fa-envelope"></i></a>
-          <a class="head-tool"><i class="fa fa-fw fa-search"></i></a>
+          <a class="head-tool">
+            <i class="fa fa-fw fa-level-down"></i>
+          </a>
+          <a class="head-tool">
+            <i class="fa fa-fw fa-tasks"></i>
+          </a>
+          <a class="head-tool">
+            <i class="fa fa-fw fa-bell"></i>
+          </a>
+          <a class="head-tool">
+            <i class="fa fa-fw fa-envelope"></i>
+          </a>
+          <a class="head-tool">
+            <i class="fa fa-fw fa-search"></i>
+          </a>
         </div>
       </el-header>
       <el-container class="frame-body">
         <el-aside width="200px" class="frame-nav">
           <el-menu default-active="1" class="el-menu-vertical-demo">
             <el-menu-item index="1">
-              <i class="fa fa-home"></i>
+              <i class="fa fa-fw fa-home"></i>
               <span slot="title">主页</span>
             </el-menu-item>
             <el-submenu index="2">
               <template slot="title">
-                <i class="fa fa-diamond"></i>
+                <i class="fa fa-fw fa-diamond"></i>
                 <span>UI 元素</span>
               </template>
               <el-menu-item index="2-1">Buttons</el-menu-item>
@@ -44,19 +52,19 @@
             </el-submenu>
             <el-submenu index="3">
               <template slot="title">
-                <i class="el-icon-location"></i>
+                <i class="fa fa-fw fa-wpforms"></i>
                 <span>表单</span>
               </template>
               <el-menu-item index="3-1">基础表单</el-menu-item>
               <el-menu-item index="3-2">表单验证</el-menu-item>
             </el-submenu>
             <el-menu-item index="4">
-              <i class="el-icon-date"></i>
+              <i class="fa fa-fw fa-table"></i>
               <span slot="title">表格</span>
             </el-menu-item>
             <el-submenu index="5">
               <template slot="title">
-                <i class="el-icon-location"></i>
+                <i class="fa fa-fw fa-file-text-o"></i>
                 <span>其他页面</span>
               </template>
               <el-menu-item index="5-1">404</el-menu-item>
@@ -74,10 +82,11 @@
               </el-breadcrumb-item>
               <el-breadcrumb-item>活动列表</el-breadcrumb-item>
             </el-breadcrumb>
+            <div class="btn-toolbar">
+              <el-button type="default" icon="el-icon-setting" size="small"></el-button>
+            </div>
           </div>
-          <div class="frame-content">
-
-          </div>
+          <div class="frame-content"></div>
         </el-main>
       </el-container>
     </el-container>
